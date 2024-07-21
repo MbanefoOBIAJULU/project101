034d4e432e5ee721ccf9ddffcacfa4dccbd09bf2
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-07-21 23:50:35 +0100

    Call Stack

---
+++ foreach.js
@@ -0,0 +1,44 @@
+/*let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+
+names.forEach(function(name) {
+  console.log(name);
+});
+
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+names.forEach(name=>console.log(name));
+
+function doubler(number) {
+    console.log(number);
+  
+    if (number <= 50) {
+      doubler(number * 2);
+    }
+  }
+  
+  doubler(5); // => 5
+              // => 10
+              // => 20
+              // => 40
+              // => 80
+              
+  
+function funcA() {
+console.log("Start funcA");
+funcB();
+console.log("End funcA");
+}
+            
+function funcB() {
+console.log("Start funcB");
+funcC();
+console.log("End funcB");
+}
+            
+function funcC() {
+console.log("Start funcC");
+console.log("End funcC");
+}
+            
+funcA();*/
+            
+
