dda6b11a5d3dba97341028ad9fe63a2b01979e7c
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-07-21 03:51:38 +0100

    Answer to uestion 9 not 10

--- project.js
+++ project.js
@@ -233,7 +233,7 @@ console.log(null ?? false);  //false
 console.log(undefined ?? (1 + 2)); // 3
 console.log((1 + 2) ?? null);   // 3
 console.log(null ?? undefined);  //undefined
-console.log(undefined ?? null);  // null
+console.log(undefined ?? null);  // null.
 
 
 /*Question 10
