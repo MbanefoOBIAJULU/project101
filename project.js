681cfa213996621133b93c643255d7d62e6d95e6
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-07-21 01:25:17 +0100

    .

--- project.js
+++ project.js
@@ -101,7 +101,27 @@ Default:
 console.log('Product not found!');
 }
 }
+barCodeScanner('113');*/
+
+function barCodeScanner(serial) {
+switch (serial) {
+case '123':
+console.log('Product1');
+case '113':
+console.log('Product2');
+case '142':
+console.log('Product3');
+default:
+console.log('Product not found!');
+}
+}
 barCodeScanner('113');
+/* the following code logs the following to the console
+'Project2' 'Project3' and 'product not found'; 
+The switch statement checks for the value of serial
+aginst the arguments labelled case.
+once a match is found, it logs it to the console but continues 
+to the next case label since there is no break statement after each case. 
 */
 
 
--- project1.js
+++ project1.js
@@ -1,26 +1,37 @@
-/*Let's improve our previous implementation of evenOrOdd. 
-Add a validation check to ensure that the argument is an integer. 
-If it isn't, the function should issue an error message and return.
-Hint 1
-You can use Number.isInteger(value) to determine whether a numeric 
-value is an integer. It returns true if the value is an integer, false otherwise.
+
+/*What does the following code log to the console, and why?
+
+function barCodeScanner(serial) {
+switch (serial) {
+case '123':
+console.log('Product1');
+case '113':
+console.log('Product2');
+case '142':
+console.log('Product3');
+Default:
+console.log('Product not found!');
+}
+}
+barCodeScanner('113');*/
+
+function barCodeScanner(serial) {
+switch (serial) {
+case '123':
+console.log('Product1');
+case '113':
+console.log('Product2');
+case '142':
+console.log('Product3');
+default:
+console.log('Product not found!');
+}
+}
+barCodeScanner('113');
+/* the following code logs the following to the console
+'Project2' 'Project3' and 'product not found'; 
+The switch statement checks for the value of serial
+aginst the arguments labelled case.
+once a match is found, it logs it to the console but continues 
+to the next case label since there is no break statement after each case. 
 */
-
-function evenOrOdd(number) {
-if (!Number.isInteger(number)) {
-console.log('Error: The argument is not an integer');
-return;
-}
-if (number % 2 === 0) {
-console.log(number + ' is even');
-} else {
-console.log(number + ' is odd');
-}
-}
-
-evenOrOdd(4);       // 4 is even
-evenOrOdd(3);       // 3 is odd
-evenOrOdd(0);       // 0 is even
-evenOrOdd(-1);      // -1 is odd
-evenOrOdd(2.5);     // Error: The argument is not an integer
-evenOrOdd('hello'); // Error: The argument is not an integer
