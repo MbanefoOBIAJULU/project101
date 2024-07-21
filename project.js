13f28d6eb8fb26a0480c30f78899aa5254cd1564
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-07-21 03:48:44 +0100

    Answer to Question 8

--- project.js
+++ project.js
@@ -173,7 +173,7 @@ the string is longer than 10 characters. Otherwise,
 it should return the original string. Example:
  change 'hello world' to 'HELLO WORLD', but don't change 'goodbye'.*/
 
- 
+
  function allCaps(word) {
 if (word.length > 10) {
 return word.toUpperCase()
@@ -189,18 +189,24 @@ console.log(allCaps('hello'))
 /* Question 8
 Write a function that logs whether an integer is 
 between 0 and 50 (inclusive), between 51 and 100
- (inclusive), greater than 100, or less than 0.
+ (inclusive), greater than 100, or less than 0.*/
 
+ function numberRange(num) {
+    if (num >= 0 && num <= 50) {
+        console.log('The number is between 0 and 50 (inclusive)');
+    } else if (num >= 51 && num <= 100) {
+        console.log('The number is between 51 and 100 (inclusive)');
+    } else if (num > 100) {
+        console.log('The number is greater than 100');
+    } else {
+        console.log('The number is less than 0');
+    }
+}
+numberRange(25);   // 25 is between 0 and 50 
+numberRange(75);   // 75 is between 51 and 100 
+numberRange(125);  // 125 is greater than 100
+numberRange(-25);  // -25 is less than 0
 
-numberRange(25);
-numberRange(75);
-numberRange(125);
-numberRange(-25);
-Expected Output
-25 is between 0 and 50
-75 is between 51 and 100
-125 is greater than 100
--25 is less than 0*/
 
 
 
@@ -215,7 +221,6 @@ console.log((1 + 2) ?? null);
 console.log(null ?? undefined);
 console.log(undefined ?? null);
 */
-
 
 /*Question 10
 Without running this code, what will it print?
--- project1.js
+++ project1.js
@@ -1,30 +1,10 @@
-/*Write a function that takes a string as
- an argument and returns an all-caps version 
- of the string when the string is longer than 
- 10 characters. Otherwise, it should return the 
- original string. Example: change 'hello world' 
- to 'HELLO WORLD', but don't change 'goodbye'.*/
 
- function allCaps(word) {
-if (word.length > 10) {
-return word.toUpperCase()
-} else{
-    return word
-}
-}
-console.log(allCaps('hello world'))
-console.log(allCaps('goodbye'))
-console.log(allCaps('hello'))
-
-/*function convertString(str) {
-    if (str.length > 10) {
-        return str.toUpperCase();
-    } else {
-        return str;
-    }
-}
-
-// Testing the function
-console.log(convertString('hello world')); // Outputs: 'HELLO WORLD'
-console.log(convertString('goodbye')); // Outputs: 'goodbye'
-*/
+/*Without running this code, what will it print?
+console.log(false ?? null);
+console.log(true ?? (1 + 2));
+console.log((1 + 2) ?? true);
+console.log(null ?? false);
+console.log(undefined ?? (1 + 2));
+console.log((1 + 2) ?? null);
+console.log(null ?? undefined);
+console.log(undefined ?? null);*/
