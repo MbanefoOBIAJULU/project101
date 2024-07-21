ec540ff208dabf5bcd9124d9d57075573e9a4398
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-07-21 03:49:54 +0100

    Answer to uestion 10

--- project.js
+++ project.js
@@ -222,6 +222,20 @@ console.log(null ?? undefined);
 console.log(undefined ?? null);
 */
 
+/*The ?? operator a nullish coalescing operator. 
+It returns the right-hand operand when the left-hand operand is null 
+or undefined; otherwise, it returns the left-hand operand.
+*/
+console.log(false ?? null);  //false
+console.log(true ?? (1 + 2));  // true
+console.log((1 + 2) ?? true);  //3
+console.log(null ?? false);  //false
+console.log(undefined ?? (1 + 2)); // 3
+console.log((1 + 2) ?? null);   // 3
+console.log(null ?? undefined);  //undefined
+console.log(undefined ?? null);  // null
+
+
 /*Question 10
 Without running this code, what will it print?
 
