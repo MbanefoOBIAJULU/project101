e52506af3a920df5c0cba2a5e2753cec09f2c132
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-08-05 12:29:39 +0100

    old method of Template literals

--- project1.js
+++ project1.js
@@ -1,10 +1,18 @@
+/*const ageCollins = 2024 - 1992;
+const ageMarculla = 2024 - 1994;
+let ageChike = 25;
+//console.log(ageCollins, ageMarculla);
 
-/*Without running this code, what will it print?
-console.log(false ?? null);
-console.log(true ?? (1 + 2));
-console.log((1 + 2) ?? true);
-console.log(null ?? false);
-console.log(undefined ?? (1 + 2));
-console.log((1 + 2) ?? null);
-console.log(null ?? undefined);
-console.log(undefined ?? null);*/
+console.log(ageCollins + ageMarculla - ageChike);
+console.log(ageCollins/ 2, ageChike - 10, ageMarculla + 50);
+ 
+let x, y;
+x = y = 25 - 10 + 5;
+console.log(x, y);*/
+
+const firstName = 'Obiajulu';
+const job = 'teacher';
+const birthYear = 1992;
+const year = 2024;
+const obiajulu = 'i' +' ' + 'am' +' ' + firstName +' ' + 'a' +' ' + (year - birthYear) +' ' + 'years' + ' ' + 'old' +' '+ job;
+console.log(obiajulu);
